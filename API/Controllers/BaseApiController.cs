65d410ead7556b5fcef42ca3b3d283d642b6a6cb
Venkatarajesh Jakka <venkatarajeshjakka@Venkatarajeshs-MacBook-Air.local>
2021-06-03 19:22:11 -0400

    added handler

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,11 +1,15 @@
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
-
+using Microsoft.Extensions.DependencyInjection;
 namespace API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
     public class BaseApiController : ControllerBase
     {
-        
+        private IMediator _meditor;
+
+        protected IMediator Mediator => _meditor ??= HttpContext.RequestServices
+        .GetService<IMediator>();
     }
 }
